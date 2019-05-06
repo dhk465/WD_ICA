c9b7207da37d1613859d782a8d3aceb17ec02289
Daehee <daehee.kim465@gmail.com>
2019-05-06 10:47:55 +0200

    added sections into the page

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
-
 	$("#menu a").on('click', function (e) {
 		e.preventDefault();
 		$('#buttons').toggleClass("toggle");
 	});
-
+	$.scrollify({
+		section: ".scroll"
+	});
+	$(".buttons").click(function (e) {
+		e.preventDefault();
+		$.scrollify("move", $(this).attr("href"));
+	});
 });
